cadaef52f85cff02994f2c2ae1d793faba150627
Nikola <nikola_vuchev@abv.bg>
2021-01-21 15:47:42 +0200

    JavaAdvanced

--- src/SetAndMaps/ProductShop.java
+++ src/SetAndMaps/ProductShop.java
@@ -1,22 +1,22 @@
 package SetAndMaps;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class ProductShop {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         String[] input = scan.nextLine().split(", ");
-        Map<String, Map<String, Double>> map = new LinkedHashMap<>();
+        Map<String, Map<String, Double>> map = new TreeMap<>();
 
         while (!input[0].equals("Revision")){
             String name = input[0];
             String product = input[1];
             double price = Double.parseDouble(input[2]);
 
-            map.putIfAbsent(name, new LinkedHashMap<>());
+            map.putIfAbsent(name, new TreeMap<>());
             map.get(name).put(product, price);
 
             input = scan.nextLine().split(", ");
