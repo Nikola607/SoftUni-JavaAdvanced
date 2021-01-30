43051329a1e78f56cdc21fe664e7e90092b216b3
Nikola <nikola_vuchev@abv.bg>
2021-01-30 17:47:34 +0200

    JavaAdvanced

--- src/MultidimensionalArrays/FindTheRealQueen.java
+++ src/MultidimensionalArrays/FindTheRealQueen.java
@@ -6,118 +6,124 @@ public class FindTheRealQueen {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int rowsAndCols = 8;
-        char[][] matrix = new char[rowsAndCols][rowsAndCols];
+        int rows = 8;
+        int cols = 8;
 
-        for (int i = 0; i < matrix.length; i++) {
-            char[] arr = scan.nextLine().replaceAll(" ", "").
-                    toCharArray();
+        char[][] chessTable = readMatrix(rows, cols, scan);
 
-            matrix[i] = arr;
-        }
+        for (int row = 0; row < chessTable.length; row++) {
+            for (int elem = 0; elem < chessTable[row].length; elem++) {
+                char currentElement = chessTable[row][elem];
 
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix.length; col++) {
-                char currentSymbol = matrix[row][col];
-                if (currentSymbol == 'q') {
-                    checkIfIsRealQueen(matrix, row, col);
-                    if (checkIfIsRealQueen(matrix, row, col)) {
-                        System.out.println(row + " " + col);
-                        break;
+                if (currentElement == 'q') {
+                    if (check(chessTable, row, elem)) {
+                        System.out.print(row + " ");
+                        System.out.print(elem);
+
                     }
+
                 }
             }
         }
+
+
     }
 
-    private static boolean checkIfIsRealQueen(char[][] matrix, int currentRow, int currentCol) {
-        if (!HorizontalLine(matrix, currentRow) || !VerticalLine(matrix, currentCol)
-                || !UpRightVerticalLine(matrix, currentRow, currentCol) || !DownRightVerticalLine(matrix, currentRow, currentCol)
-        || !UpLeftVerticalLine(matrix, currentRow, currentCol) || !DownLeftVerticalLine(matrix, currentRow, currentCol)) {
-            return false;
+    private static char[][] readMatrix(int rows, int cols, Scanner scan) {
+        char[][] matrix = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+
+            String[] line = scan.nextLine().split("\\s+");
+            for (int col = 0; col < matrix[row].length; col++) {
+                char currentChar = line[col].charAt(0);
+                matrix[row][col] = currentChar;
+            }
+
         }
+
+
+        return matrix;
+    }
+
+    private static boolean check(char[][] chessTable, int row, int elem) {
+
+        for (int i = elem + 1; i < chessTable[row].length; i++) { //RightHorizontal yes
+            char charToCheck = chessTable[row][i];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+        for (int i = elem - 1; i >= 0; i--) { //LeftHorizontal yes
+            char charToCheck = chessTable[row][i];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+
+        for (int i = row + 1; i < chessTable.length; i++) { //DownVertical yes
+            char charToCheck = chessTable[i][elem];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1; i >= 0; i--) { // UpVertical yes
+            char charToCheck = chessTable[i][elem];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+        int uldElement = elem - 1;
+
+        for (int i = row - 1; i >= 0 && uldElement >= 0; i--, uldElement--) { // UpLeftDiagonal yes
+            char charToCheck = chessTable[i][uldElement];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+
+        int urdElement = elem + 1;
+
+        for (int i = row - 1; i >= 0 && urdElement < chessTable[i].length; i--, urdElement++) { // UpRightDiagonal yes
+            char charToCheck = chessTable[i][urdElement];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+
+        int element = elem - 1;
+
+        for (int i = row + 1; i < chessTable.length && element >= 0; i++, element--) { // DownLeftDiagonal
+            char charToCheck = chessTable[i][element];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
+        int element1 = elem + 1;
+
+        for (int i = row + 1; i < chessTable.length && element1 < chessTable[row].length; i++, element1++) { // DownRightDiagonal
+            char charToCheck = chessTable[i][element1];
+
+            if (charToCheck == 'q') {
+                return false;
+            }
+        }
+
         return true;
     }
 
-    private static boolean UpLeftVerticalLine(char[][] matrix, int currentRow, int currentCol) {
-        int queenCounter = 0;
-
-        for (int row = currentRow; row >= 0; row--) {
-            for (int col = currentCol; col >= 0; col--) {
-                char currentSymbol = matrix[row][currentCol];
-
-                if (row + col == matrix.length - 1 && currentSymbol == 'q') {
-                    queenCounter++;
-                }
-            }
-        }
-        return queenCounter < 2;
-    }
-    private static boolean DownLeftVerticalLine(char[][] matrix, int currentRow, int currentCol) {
-        int queenCounter = 0;
-
-        for (int row = currentRow; row < matrix.length; row++) {
-            for (int col = currentCol; col < matrix[row].length; col++) {
-                char currentSymbol = matrix[row][currentCol];
-
-                if (row + col == matrix.length - 1 && currentSymbol == 'q') {
-                    queenCounter++;
-                }
-            }
-        }
-        return queenCounter < 2;
-    }
-
-    private static boolean UpRightVerticalLine(char[][] matrix, int currentRow, int currentCol) {
-        int queenCounter = 0;
-
-        for (int row = currentRow; row >= 0; row--) {
-            for (int col = currentCol; col >= 0; col--) {
-                char currentSymbol = matrix[row][currentCol];
-
-                if (row == col && currentSymbol == 'q') {
-                    queenCounter++;
-                }
-            }
-        }
-        return queenCounter < 2;
-    }
-    private static boolean DownRightVerticalLine(char[][] matrix, int currentRow, int currentCol) {
-        int queenCounter = 0;
-
-        for (int row = currentRow; row < matrix.length; row++) {
-            for (int col = currentCol; col < matrix[row].length; col++) {
-                char currentSymbol = matrix[row][currentCol];
-
-                if (row == col && currentSymbol == 'q') {
-                    queenCounter++;
-                }
-            }
-        }
-        return queenCounter < 2;
-    }
-
-    private static boolean VerticalLine(char[][] matrix, int currentCol) {
-        int queenCounter = 0;
-
-        for (int row = 0; row < matrix.length; row++) {
-            char currentSymbol = matrix[row][currentCol];
-            if (currentSymbol == 'q') {
-                queenCounter++;
-            }
-        }
-        return queenCounter < 2;
-    }
-
-    private static boolean HorizontalLine(char[][] matrix, int currentRow) {
-        int queenCounter = 0;
-
-        for (int col = 0; col < matrix[currentRow].length; col++) {
-            char currentSymbol = matrix[currentRow][col];
-            if (currentSymbol == 'q') {
-                queenCounter++;
-            }
-        }
-        return queenCounter < 2;
-    }
 }
