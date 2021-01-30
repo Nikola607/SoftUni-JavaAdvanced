e8e78b734ef77d2ccd117994f3ab4ac78c19b54b
Nikola <nikola_vuchev@abv.bg>
2021-01-30 17:25:00 +0200

    JavaAdvanced

--- src/MultidimensionalArrays/FindTheRealQueen.java
+++ src/MultidimensionalArrays/FindTheRealQueen.java
@@ -21,7 +21,7 @@ public class FindTheRealQueen {
                 char currentSymbol = matrix[row][col];
                 if (currentSymbol == 'q') {
                     checkIfIsRealQueen(matrix, row, col);
-                    if(checkIfIsRealQueen(matrix, row, col)){
+                    if (checkIfIsRealQueen(matrix, row, col)) {
                         System.out.println(row + " " + col);
                         break;
                     }
@@ -31,16 +31,76 @@ public class FindTheRealQueen {
     }
 
     private static boolean checkIfIsRealQueen(char[][] matrix, int currentRow, int currentCol) {
-        if(!HorizontalLine(matrix, currentRow, currentCol) || !VerticalLine(matrix, currentRow, currentCol)){
+        if (!HorizontalLine(matrix, currentRow) || !VerticalLine(matrix, currentCol)
+                || !UpRightVerticalLine(matrix, currentRow, currentCol) || !DownRightVerticalLine(matrix, currentRow, currentCol)
+        || !UpLeftVerticalLine(matrix, currentRow, currentCol) || !DownLeftVerticalLine(matrix, currentRow, currentCol)) {
             return false;
         }
-        return  true;
+        return true;
     }
 
-    private static boolean VerticalLine(char[][] matrix, int currentRow, int currentCol) {
+    private static boolean UpLeftVerticalLine(char[][] matrix, int currentRow, int currentCol) {
         int queenCounter = 0;
 
-        for (int row = 0; row <matrix.length; row++) {
+        for (int row = currentRow; row >= 0; row--) {
+            for (int col = currentCol; col >= 0; col--) {
+                char currentSymbol = matrix[row][currentCol];
+
+                if (row + col == matrix.length - 1 && currentSymbol == 'q') {
+                    queenCounter++;
+                }
+            }
+        }
+        return queenCounter < 2;
+    }
+    private static boolean DownLeftVerticalLine(char[][] matrix, int currentRow, int currentCol) {
+        int queenCounter = 0;
+
+        for (int row = currentRow; row < matrix.length; row++) {
+            for (int col = currentCol; col < matrix[row].length; col++) {
+                char currentSymbol = matrix[row][currentCol];
+
+                if (row + col == matrix.length - 1 && currentSymbol == 'q') {
+                    queenCounter++;
+                }
+            }
+        }
+        return queenCounter < 2;
+    }
+
+    private static boolean UpRightVerticalLine(char[][] matrix, int currentRow, int currentCol) {
+        int queenCounter = 0;
+
+        for (int row = currentRow; row >= 0; row--) {
+            for (int col = currentCol; col >= 0; col--) {
+                char currentSymbol = matrix[row][currentCol];
+
+                if (row == col && currentSymbol == 'q') {
+                    queenCounter++;
+                }
+            }
+        }
+        return queenCounter < 2;
+    }
+    private static boolean DownRightVerticalLine(char[][] matrix, int currentRow, int currentCol) {
+        int queenCounter = 0;
+
+        for (int row = currentRow; row < matrix.length; row++) {
+            for (int col = currentCol; col < matrix[row].length; col++) {
+                char currentSymbol = matrix[row][currentCol];
+
+                if (row == col && currentSymbol == 'q') {
+                    queenCounter++;
+                }
+            }
+        }
+        return queenCounter < 2;
+    }
+
+    private static boolean VerticalLine(char[][] matrix, int currentCol) {
+        int queenCounter = 0;
+
+        for (int row = 0; row < matrix.length; row++) {
             char currentSymbol = matrix[row][currentCol];
             if (currentSymbol == 'q') {
                 queenCounter++;
@@ -49,7 +109,7 @@ public class FindTheRealQueen {
         return queenCounter < 2;
     }
 
-    private static boolean HorizontalLine(char[][] matrix, int currentRow, int currentCol) {
+    private static boolean HorizontalLine(char[][] matrix, int currentRow) {
         int queenCounter = 0;
 
         for (int col = 0; col < matrix[currentRow].length; col++) {
