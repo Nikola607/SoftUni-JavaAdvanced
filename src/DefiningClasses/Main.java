0413582377869f502b7b8270f167969121444336
Nikola <nikola_vuchev@abv.bg>
2021-02-01 12:37:16 +0200

    JavaAdvanced

--- src/DefiningClasses/Main.java
+++ src/DefiningClasses/Main.java
@@ -10,13 +10,20 @@ public class Main {
 
         String[] info = scan.nextLine().split("\\s+");
 
-        while (n-- > 0){
+        while (n-- > 0) {
+            CarConstructor car;
             String brand = info[0];
-            String model = info[1];
-            int horsePower =Integer.parseInt(info[2]);
 
-            CarInfo car = new CarInfo(brand, model, horsePower);
-            System.out.printf("The car is: %s %s - %d\n", car.getBrand(), car.getModel(), car.getHorsePower());
+            if (info.length == 1) {
+                car = new CarConstructor(brand);
+            }else {
+                String model = info[1];
+                int horsePower = Integer.parseInt(info[2]);
+
+                car = new CarConstructor(brand, model, horsePower);
+            }
+            System.out.printf("The car is: %s %s - %d HP.\n", car.getBrand(), car.getModel(), car.getHorsePower());
+
             info = scan.nextLine().split("\\s+");
         }
     }
